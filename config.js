f3222388bcf539e5bc5c9b532d89c19f891b0e67
Leo Mercier <Home@Leos-MacBook-Air.local>
2017-06-15 15:28:41 +0100

    updates to parse w/ dashboard

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  appName: process.env.APP_NAME || 'LoopServer',
+  databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/createreactappparse', // Connection string for your MongoDB database
+  cloud: __dirname + '/cloud/main.js', // Absolute path to your Cloud Code
+  appId: process.env.APP_ID || 'loop-server',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', // Keep this key secret!
+  fileKey: 'optionalFileKey',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse' // Don't forget to change to https if needed
+}
--- index.js
+++ index.js
@@ -1,55 +1,37 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+import express from 'express';
+import { ParseServer } from 'parse-server';
+import ParseDashboard from 'parse-dashboard';
+import config from './config';
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+const app = express();
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+const PORT = process.env.PORT || 1337;
 
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+const api = new ParseServer(config);
+
+app.use('/parse', api);
+
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: config.serverURL,
+      appId: config.appId,
+      masterKey: config.masterKey,
+      appName: config.appName
+    }
+  ]
+});
+
+if(process.env.NODE_ENV !== 'production') {
+  app.use('/dashboard', dashboard);
+  console.info(`dashboard available at http://localhost:${PORT}/dashboard`); // eslint-disable-line no-console
 }
 
-var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+app.get('/', (req, res) => res.redirect(301, '/parse'));
+
+app.listen(PORT, (err) => {
+  if(err) {
+    return console.error(err);
   }
+  console.log(`parse api listening on ${config.serverURL}`);
 });
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
-
-var app = express();
-
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-
-// Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
-
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
-
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
-});
-
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
