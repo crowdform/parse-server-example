6d401d14748ee0a792331e5e8090fa20e11277a7
Leo Mercier <leo@crowdform.co.uk>
2017-07-04 18:20:30 +0100

    server query code

--- server/api/index.js
+++ server/api/index.js
@@ -11,16 +11,68 @@ mongo.connect(mongoDB).then( (database) => {
   // ... start the server
   db = database;
   console.log("MongoDB Connected");
-  console.log('Query');
-  db.collection("Venue").find().then(function(docs) {
-    console.log(docs);
-  });
+  // console.log('Query');
+  // db.collection("Venue").find().then(function(docs) {
+  //   console.log(docs);
+  // });
 }).catch((err) => {
   console.error("No Database Connection");
 });
 
 export default (req, res) => {
-  return res.json({
-    result: false
-  })
+  // Find properties on Monday that are open between 10am and 1pm
+
+  // the mega query
+  // query location
+  // query types
+  // query dates
+  const Venue = db.collection("Venue");
+  const Spaces = db.collection("Space");
+
+  //
+  // Spaces.find({
+  //   "_p_venue": 'Venue$FOyoeCynTs'
+  // }).then((spaces) => {
+  //   console.log('Spaces', spaces);
+  // });
+
+  Venue.find({
+    availability: {
+      $elemMatch : {
+        "day": 0,
+        "to" : { $gte: "09"},
+        "from": { $lte: "16"},
+      }
+    }
+  }, { _id: 1 }).then(function(venues) {
+    // parse pointer fix
+    const venueArr = venues.map(function(venue){ return 'Venue$' + venue._id; });
+    // find spaces matching the query
+    console.log(venueArr);
+    return Spaces.find({
+      "_p_venue": {
+        $in : venueArr
+      }
+    }, { _p_venue: 1 });
+  }).then((spaces) => {
+    console.log('Spaces', spaces);
+    // find the venues from available spaces
+    const venueArr = spaces.map(function(item){ return  item._p_venue.replace('Venue$', ''); });
+    // remove duplicates
+    const venues = venueArr.filter( function( item, index, inputArray ) {
+          return inputArray.indexOf(item) == index;
+    });
+    return venues;
+  }).then((result) => {
+    // return full list of venues again
+    console.log('Venues with space', result)
+    Venue.find({
+      _id: {   $in : result }
+    }).then((results) => {
+      return res.json({
+        results
+      })
+    });
+  });
+
 }
